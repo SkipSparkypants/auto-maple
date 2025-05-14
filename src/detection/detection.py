2485e4c8d0fcd14058a1f4a55427c20c2b8c4779
SkipSparkypants <agentalexiscool@yahoo.com>
2025-05-13 21:36:45 -0700

    fix model

--- src/detection/detection.py
+++ src/detection/detection.py
@@ -22,10 +22,8 @@ def load_model():
     Loads the saved model's weights into an Tensorflow model.
     :return:    The Tensorflow model object.
     """
-    # model_dir = f'assets/models/rune_model_rnn_filtered_cannied/saved_model'
-    # return tf.saved_model.load(model_dir)
-    return None
-
+    model_dir = f'assets/models/rune_model_rnn_filtered_cannied/saved_model'
+    return tf.saved_model.load(model_dir)
 
 def canny(image):
     """
